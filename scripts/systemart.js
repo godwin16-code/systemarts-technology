e5e4f0cff5652d0c49df58550d85661317e84e91
godwin16-code <Iwajomogodwin72@gmail.com>
2026-06-27 01:40:22 +0100

    added menu-overlay to the header

--- scripts/systemart.js
+++ scripts/systemart.js
@@ -1,17 +1,30 @@
 const hamburgerIcon = document.getElementById("hamburger");
 const navPanel = document.getElementById("script");
 const closeButton = document.querySelector(".navigationX button");
+const overlay = document.getElementById("overlay");
 
-if (hamburgerIcon && navPanel) {
-    hamburgerIcon.addEventListener("click", () => {
-        hamburgerIcon.classList.toggle("open");
-        navPanel.classList.toggle("open");
-    });
+function toggleMenu() {
+    hamburgerIcon.classList.toggle("open");
+    navPanel.classList.toggle("open");
+    overlay.classList.toggle("open");
 }
 
-if (closeButton && navPanel && hamburgerIcon) {
+if (hamburgerIcon && navPanel && overlay) {
+    hamburgerIcon.addEventListener("click", toggleMenu);
+}
+
+if (closeButton && navPanel && hamburgerIcon && overlay) {
     closeButton.addEventListener("click", () => {
         navPanel.classList.remove("open");
         hamburgerIcon.classList.remove("open");
+        overlay.classList.remove("open");
+    });
+}
+
+if (overlay) {
+    overlay.addEventListener("click", () => {
+        navPanel.classList.remove("open");
+        hamburgerIcon.classList.remove("open");
+        overlay.classList.remove("open");
     });
 }
